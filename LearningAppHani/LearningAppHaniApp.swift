ba7ad35cd337a62ac6633315b3e417eca4d62017
Hani <hanacsek@gmail.com>
2023-06-03 07:18:02 +0200

    Initial Commit

---
+++ LearningAppHani/LearningAppHaniApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppHaniApp.swift
+//  LearningAppHani
+//
+//  Created by Hanácsek Gábor on 2023. 06. 03..
+//
+
+import SwiftUI
+
+@main
+struct LearningAppHaniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
